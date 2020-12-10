dea17f30f931d087597743be3e0cfd1d02580088
Alejandro Baez <nativo.alejandro@gmail.com>
2020-12-10 17:22:39 -0500

    added twcss

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Header from '../src/components/Header';
+
+function App() {
+  return (
+    <div className="h-screen bg-gray-200">
+      <Header />
+    </div>
+  );
+}
+
+export default App;

4cc6aa4f988a605af893b26920933d8a237a9557
Alejandro Baez <nativo.alejandro@gmail.com>
2020-12-16 15:22:07 -0500

    added styles to profile card

--- frontend/src/components/People/PeopleView.js
+++ frontend/src/components/People/PeopleView.js
@@ -4,7 +4,7 @@ function PeopleView() {
 	return (
 		<main id="projects-view" className="h-screen overflow-auto">
 			<p className="text-5xl text-gray-900 md:mt-40 lg:mx-32 mb-2">People</p>
-			<section id="section-projects" className="flex flex-col my-20 mx-2 sm:my-0 sm:justify-center sm:mx-6 sm:flex-row sm:flex-wrap md:mx-10 lg:mx-24 2xl:mx-64">
+			<section id="section-projects" className="flex flex-col my-20 mx-2 sm:my-0  sm:mx-6 sm:flex-row sm:flex-wrap md:mx-10 lg:mx-32 2xl:mx-64">
 				<ProfileCard />
 				<ProfileCard />
 				<ProfileCard />

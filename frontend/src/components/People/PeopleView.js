198dab6c51ed01bb15117b7ad57d646457be06f4
Alejandro Baez <nativo.alejandro@gmail.com>
2020-12-25 12:08:15 -0500

    added tablet styles for projects

--- frontend/src/components/People/PeopleView.js
+++ frontend/src/components/People/PeopleView.js
@@ -3,7 +3,7 @@ import ProfileCard from './ProfileCard';
 function PeopleView({ people }) {
 	return (
 		<main id="people-view" className="h-screen overflow-auto">
-			<p className="text-center text-5xl mt-32 md:mx-10 lg:text-left lg:mx-32 mb-2">People</p>
+			<p className="text-center text-5xl mt-32 md:mx-10 lg:text-left lg:mx-32 mb-2 md:mb-6">People</p>
 			<section id="section-people" className="flex flex-col justify-center mx-6 mb-4 sm:my-0 sm:mx-6 sm:flex-row sm:flex-wrap md:mx-10 lg:mx-32 2xl:mx-64">
 				{people && people.list && people.list.map(p => <ProfileCard key={p.id} person={p} />)}
 			</section>

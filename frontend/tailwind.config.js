59676bcaaf930f5339598eec376436c3a64b14a5
Alejandro Baez <nativo.alejandro@gmail.com>
2020-12-24 23:09:14 -0500

    added roboto font family

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,11 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Roboto', 'Helvetica', 'Arial', 'sans-serif']
+      }
+    },
   },
   variants: {
     extend: {
       backgroundColor: ['active'],
+      textColor: ['active'],
+      opacity: ['disabled'],
     }
   },
   plugins: [],

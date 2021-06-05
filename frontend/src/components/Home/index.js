6ff1bb0303256af01058d68c015c5c1b26f2378c
Alejandro Baez <nativo.alejandro@gmail.com>
2021-06-05 18:59:32 -0500

    removed opacity

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -17,9 +17,9 @@ function RecentProjects(arr, people) {
 
 function Banner() {
 	return (
-		<div className="flex flex-col items-center bg-white rounded sm:shadow-sm mb-10 lg:pt-8 lg:pb-12">
-			<img className="w-32 h-32 opacity-75" src="/images/logo.png" alt="logo" />
-			<p className="opacity-50 mt-4 text-xl text-center font-light sm:px-10 sm:text-justify lg:text-center lg:text-lg leading-relaxed tracking-wide">Opentechs is a repository of projects developed by the group of people posted on the website. Here you will find the latest works created by our team along with complementary information on each project.</p>
+		<div className="flex flex-col items-center bg-white rounded sm:shadow-md mb-10 lg:pt-8 lg:pb-12">
+			<img className="w-32 h-32" src="/images/logo.png" alt="logo" />
+			<p className="opacity-50 mt-4 text-xl text-center font-light sm:px-10 sm:text-justify lg:text-center lg:text-lg lg:px-16 leading-relaxed tracking-wide">Opentechs is a repository of projects developed by the group of people posted on the website. Here you will find the latest works created by our team along with complementary information on each project.</p>
 		</div>
 	)
 }
